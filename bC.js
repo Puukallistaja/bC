4019987b394ff4850df5208889ae2eae5e0393ad
Arto Haponen <haponen@gmail.com>
2019-05-21 21:51:29 +0200

    Genesis block will contain all required methods for the bC functioning

--- bC.js
+++ bC.js
@@ -3,15 +3,22 @@ const Chain = require('./C')
 
 const validateData = x => x
 const getLatestHash = () => Chain.length ? Chain.slice(-1)[0].hash : '0'
+const stringifyChainMethod = method => ""+method
+
+const genesisBlock = {
+  hash: 0,
+  validator: stringifyChainMethod(validateData),
+}
 
 module.exports = () => {
-  Chain.push({
+  if (!Chain.length) { Chain.push(genesisBlock) }
 
-  })
+  const validate = eval(genesisBlock.validator)
+  
   return {
     link: data => Chain.push(block({
       latestHash: getLatestHash(),
-      data: validateData({data})
+      data: validate({data})
     })),
     get latestBlock() {
       return Chain.slice(-1)[0]
