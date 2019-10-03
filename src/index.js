1ca53460c590f89bd494f931415e7a71e91ebf71
Arto Haponen <arto@haponen.com>
2019-10-03 19:39:44 +0200

    chain(index.js)

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const spawn = require('child_process').spawn
+
+module.exports.bC = {
+  split(from) {
+    return
+  },
+  chain(block) {
+    return
+  },
+  join(to) {
+    return
+  },
+  follow(chain) {
+    return
+  },
+  start(name) {
+    return
+  },
+}
