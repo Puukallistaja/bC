a3fa46a68090471f62313f636d9211f65836259f
Arto Haponen <arto@haponen.com>
2019-10-08 19:33:44 +0200

    Add function wrapper for stdout

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
-const spawn = require('child_process').spawn
+const spawn = require("child_process").spawn
+const fs = require("fs-extra")
+
+const piper = fn => (cmd, ...args) => fn(cmd, args).stdout.pipe(process.stdout)
+const cmd = piper(spawn)
 
 module.exports.bC = {
-  split(from) {
-    return
-  },
-  chain(block) {
-    return
-  },
-  join(to) {
-    return
-  },
-  follow(chain) {
-    return
-  },
-  start(name) {
-    return
-  },
+	split(from) {
+		return
+	},
+	chain({ message, file }) {
+		return
+	},
+	join(to) {
+		return
+	},
+	follow(chain) {
+		return
+	},
+	delete(chain) {},
+	async start(name) {
+		const chainName = "CHAINS/" + name
+
+		await fs.ensureDir(chainName)
+
+		cmd("git", "init", chainName)
+		cmd("git", "-C", chainName, "status")
+		cmd("ls", "CHAINS")
+	},
 }
