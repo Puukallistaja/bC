367459d958a7af3248dbf358823ddb0e58c8f1b8
Arto Haponen <arto@haponen.com>
2019-10-30 22:25:16 +0100

    Add transform

---
+++ START.js
@@ -0,0 +1,4 @@
+require("./src/index.js").bC.chain({
+  chainName: "bC",
+  filePath: "../Downloads/mineral moon.png"
+})
--- src/index.js
+++ src/index.js
@@ -1,35 +1,53 @@
+const fs = require("fs-extra")
+const stream = require("stream")
 const spawn = require("child_process").spawn
 const hash = require("crypto")
-	.createHash("sha256")
-	.setEncoding("hex")
-const fs = require("fs-extra")
+  .createHash("sha256")
+  .setEncoding("hex")
+const pass = new stream.PassThrough()
 
 const piper = fn => (cmd, ...args) => fn(cmd, args).stdout.pipe(process.stdout)
 const cmd = piper(spawn)
 
+const isEqual = (...x) => x.every(element => element === x[0])
+const makeBlock = ({ headHash, dataHash }) => {}
+
 module.exports.bC = {
-	split(from) {
-		return
-	},
-	chain({ message, filePath }) {
-		fs.createReadStream(filePath)
-			.pipe(hash)
-			.pipe(process.stdout)
-	},
-	join(to) {
-		return
-	},
-	follow(chain) {
-		return
-	},
-	delete(chain) {},
-	async start(name) {
-		const chainName = "CHAINS/" + name
+  split(from) {
+    return
+  },
+  chain({ chainName, filePath }) {
+    ;[...[filePath]].map(path =>
+      fs
+        .createReadStream(path)
+        // .pipe(
+        //   new stream.Transform({
+        //     transform(chunk, encoding, callback) {
+        //       console.log(chunk)
+        //       this.push(chunk)
+        //     }
+        //   })
+        // )
+        .pipe(hash)
+        .pipe(fs.createWriteStream(`./CHAINS/${chainName}/Head.bC`))
+    )
+  },
+  join(to) {
+    return
+  },
+  follow(chain) {
+    return
+  },
+  delete(chain) {
+    return
+  },
+  async start(name, secret) {
+    const chainName = "CHAINS/" + name
 
-		await fs.ensureDir(chainName)
+    await fs.ensureDir(chainName)
 
-		cmd("git", "init", chainName)
-		cmd("git", "-C", chainName, "status")
-		cmd("ls", "CHAINS")
-	},
+    cmd("git", "init", chainName)
+    cmd("git", "-C", chainName, "status")
+    cmd("ls", "CHAINS")
+  }
 }
